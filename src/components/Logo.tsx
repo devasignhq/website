fc6a03ce1c6f5304dd092ba65f2c089fc31f9405
lenny_malcolm0 <leonardmalcolm00@gmail.com>
2025-12-09 14:42:08 +0100

    refactor: update logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,13 +3,11 @@ import iconBlack from "../assets/icon-black.png";
 export function Logo() {
     return (
         <div className="flex gap-[5.717px] items-center">
-            <div className="h-[30px] w-[30px] relative shrink-0">
-                <img
-                    src={iconBlack}
-                    alt="DevAsign Icon"
-                    className="block size-full object-contain"
-                />
-            </div>
+            <img
+                src={iconBlack}
+                alt="DevAsign Icon"
+                className="h-[30px] w-[30px] object-cover"
+            />
             <p className="font-geist-ultrablack leading-normal text-xl md:text-3xl text-white tracking-[-0.5px] md:tracking-[-1.6909px] whitespace-pre">DevAsign</p>
         </div>
     );
