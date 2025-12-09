696e8fefa45e1721b9d7b93e42cec218bd1847d7
lenny_malcolm0 <leonardmalcolm00@gmail.com>
2025-12-09 14:40:03 +0100

    refactor: update Logo component to use DevAsign icon png

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,14 @@
-import svgPaths from "../imports/svg-mzil336qsi";
+import iconBlack from "../assets/icon-black.png";
 
 export function Logo() {
     return (
         <div className="flex gap-[5.717px] items-center">
             <div className="h-[30px] w-[30px] relative shrink-0">
-                <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 31 30">
-                    <path d={svgPaths.p177efd00} fill="#FFC795" />
-                    <path clipRule="evenodd" d={svgPaths.pffa9c00} fill="#FE891F" fillRule="evenodd" />
-                </svg>
+                <img
+                    src={iconBlack}
+                    alt="DevAsign Icon"
+                    className="block size-full object-contain"
+                />
             </div>
             <p className="font-geist-ultrablack leading-normal text-xl md:text-3xl text-white tracking-[-0.5px] md:tracking-[-1.6909px] whitespace-pre">DevAsign</p>
         </div>
